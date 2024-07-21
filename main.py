97cd6620f6610165cdcb9f944708438693f3f4fc
Kelly-Kurowski <k.kurowski@students.uu.nl>
2024-07-21 11:12:38 +0200

    Commit classes

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 import pygame
 import random
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('Images/player.png').convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2()
+        self.speed = 300
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print("fire laser")
+
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)))
+
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 pygame.display.set_caption('Space shooter')
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100, 200))
 surf.fill('orange')
 x = 100
 
-# importing an image
-player_surf = pygame.image.load('Images/player.png').convert_alpha()
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = 1
+all_sprites = pygame.sprite.Group()
+star_surf = pygame.image.load('Images/star.png').convert_alpha()
+for i in range(20):
+    Star(all_sprites, star_surf)
+player = Player(all_sprites)
 
 meteor_surf = pygame.image.load('Images/meteor.png').convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
@@ -24,31 +54,23 @@ meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 
 laser_surf = pygame.image.load('Images/laser.png').convert_alpha()
 laser_rect = laser_surf.get_frect(center = (20, WINDOW_HEIGHT - 20))
 
-star_surf = pygame.image.load('Images/star.png').convert_alpha()
-star_positions = [(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)) for i in range(20)]
-
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event, 500)
 
 while running:
+    dt = clock.tick() / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == meteor_event:
+        #     print("meteor")
+
+    all_sprites.update(dt)
 
     # draw the game
     display_surface.fill('darkgrey')
-    for pos in star_positions:
-        display_surface.blit(star_surf, pos)
-
-
-    player_rect.x += player_direction * 0.4
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
-
-
-    display_surface.blit(meteor_surf, meteor_rect)
-    display_surface.blit(player_surf, player_rect)
-    display_surface.blit(laser_surf, laser_rect)
-
+    all_sprites.draw(display_surface)
     pygame.display.update()
 
 pygame.quit()
